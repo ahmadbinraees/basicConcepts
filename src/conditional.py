0a91eb99cb03030d8819edfee57665532ca78014
root <root@localhost>
2022-04-19 13:08:58 +0600

    auto publish 1650352138544

---
+++ src/conditional.py
@@ -0,0 +1,41 @@
+#if else
+lg_price = 100
+sm_price = 500
+nk_price = 300
+se_price = 400
+iphone = 1000
+
+budget = 900
+budget = 200
+
+if budget > sm_price:
+  print("I will buy samsung")
+
+elif budget >= lg_price:
+  print("I wii buy LG")
+
+else:
+  print("I am happy with nokia")
+
+#my conditions what i m thinking now. isn't over.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
