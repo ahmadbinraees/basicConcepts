fe12bca97e6a60fea00671ca73a1c52be88e1c90
root <root@localhost>
2022-04-20 01:50:31 +0600

    auto publish 1650397831539

---
+++ src/variables.py
@@ -0,0 +1,34 @@
+#converting numeric value
+
+lg_price = "45"
+sm_price = "56"
+
+plus = lg_price + "23"
+
+int_value = int(lg_price) + 1
+
+print(lg_price)
+print(sm_price)
+print(int_value + 2)
+print(plus)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
