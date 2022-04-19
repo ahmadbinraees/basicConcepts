5a7d2cc6ec19749b454e9c1de6f6ca5b4e7a5d8c
root <root@localhost>
2022-04-19 12:27:03 +0600

    auto publish 1650349622622

---
+++ src/basic.py
@@ -0,0 +1,76 @@
+
+#this is how to get code output in py
+
+print(
+"This is my first code in python. Hello python")
+
+#this is how to declare a variable
+# variable has a name then after an equal sign the value
+subject_name = "Tom"
+another_name = "Jerry"
+there_shows_name = subject_name + " & " + another_name
+
+print("This is a cartoon show name:", there_shows_name)
+#that was a string type variable
+#there are 3 types of variable
+# variable types = numaric , string , boolean
+
+#now will write numeric type of variable.
+zero = 0
+one = 1
+first_digit = 2
+second_digit = 4
+third_digit = 6
+fourth_digit = 8
+
+#now i'm going to create a math operation with these variable
+plus_result = first_digit + zero
+minus_result = fourth_digit - third_digit
+multiply_result = first_digit * one
+divided_result = second_digit / first_digit
+
+print(plus_result)
+print(minus_result)
+print(multiply_result)
+print(divided_result)
+#all operation seems good
+
+#now i'm going to write boolean types of variable
+
+is_cold = False
+is_hot = True
+
+print(is_cold , "&" , is_hot)
+
+#three types variable was completely showed
+
+#now i'm going to learn decition/ condition making
+#in_ proggraming its call if else 
+
+#now i'm going to write some if else condition
+
+mobilePrice = 5000
+haveMoney = 4999
+
+if mobilePrice < haveMoney:
+  print("I will buy a mobile")
+
+else:
+  print("calculator is better")
+
+#this condition making code is work
+
+#now we should play with if else.. .. ..
+
+
+
+
+
+
+
+
+
+
+
+
+
